1e55792cf667eba6630555cd8c6482dd8ce32fd3
Harysgithub <110060101+Harysgithub@users.noreply.github.com>
2024-08-15 15:12:31 +0530

    Add files via upload

---
+++ ASCII.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a number: "))
+print("For ASCII value", a, "character is", chr(a))
+b = (input("Enter an alphabet: "))
+print("For character", b, "ASCII value is", ord(b))
+if 'a' <= b <= 'z':
+    print("And its in Lower Case")
+elif "A" <= b <= "Z":
+    print("And its in Upper Case")
+else:
+    print("And its indifferent")
